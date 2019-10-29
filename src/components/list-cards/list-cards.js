f68ae6cfa5f16ef18ac5e7cd9dd5d8379db2ae09
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2019-10-29 18:15:24 +0100

    no message

--- src/components/list-cards/list-cards.js
+++ src/components/list-cards/list-cards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ListCards = () => {
+const ListCardsComponent = () => {
     return (
         <div>
             List Cards
@@ -8,4 +8,4 @@ const ListCards = () => {
     )
 }
 
-export default ListCards;
+export default ListCardsComponent;
