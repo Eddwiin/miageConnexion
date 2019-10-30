0dba3b6e8ae69a604b94c7ac241b379dfffcc86b
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2019-10-30 11:10:50 +0100

    Add  header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './header.scss';
 
 const HeaderComponent = () => {
     return (
-        <div>
-            Header Component
-        </div>
+        <Router>
+            <div className="header-container">
+                <header className="header">
+                    <div className="logo"><Link className="navbar-brand">Bouesso</Link></div>
+                    <div className="spacer"></div>
+                </header>
+            </div>
+        </Router>
     )
 }
 
