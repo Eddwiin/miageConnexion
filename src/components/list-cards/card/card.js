9e01ce306f155e88e0b63f41020d977b8385bcbc
doodood <abdou.soumah@gmail.com>
2019-10-31 16:07:05 +0100

    100vh

--- src/components/list-cards/card/card.js
+++ src/components/list-cards/card/card.js
@@ -4,7 +4,7 @@ import Aux from "../../../hoc/Helper";
 const CardComponent = props => (
   <Aux>
       <div className="card">
-        <img className="card__image" src="http://via.placeholder.com/640x360" />
+        <img className="card__image" src="http://via.placeholder.com/640x360" height="100vh" />
         <figcaption className="card__caption">
           Short desc of the image
         </figcaption>
