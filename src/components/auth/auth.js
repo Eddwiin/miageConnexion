5b21b6e95602bc8cdda3bdc12dea383c68784dc7
maguran.mahendrarajan@gmail.com <maguran.mahendrarajan@gmail.com>
2019-12-01 13:47:38 +0100

    Init api

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,25 +1,24 @@
-import  React, { lazy }  from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { lazy } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-const LoginComponent = lazy(() => import('./login'));
+const LoginComponent = lazy(() => import("./login"));
 
 export default class AuthContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    loadRoutes() {
-        return (
-            <Switch> 
-                <Route exact path="/login" component={LoginComponent}> </Route>
-                <Redirect from="/" to="/auth/login"></Redirect>
-            </Switch>
-        )
-    }
+  loadRoutes() {
+    return (
+      <Switch>
+        <Route path="/login" component={LoginComponent}></Route>
+        <Redirect from="/" to="/auth/login"></Redirect>
+      </Switch>
+    );
+  }
 
-
-    render() {
-        return(
-            <div>
-                { this.loadRoutes() }
-            </div>
-        )
-    }
-} 
+  render() {
+    return <div>{this.loadRoutes()}</div>;
+  }
+}
