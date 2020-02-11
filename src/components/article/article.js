397bbf0affbaec94d8609e4594e82e08c9dc4714
doodood <abdou.soumah@gmail.com>
2020-02-11 14:58:09 +0100

    add storyblok and article component

---
+++ src/components/article/article.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+import './article.scss'
+import StoryblokClient from 'storyblok-js-client'
+class Article extends Component {
+  state = {
+    articles: []
+  };
+  componentDidMount() {
+    const client = new StoryblokClient({
+        accessToken: 'ilnbvJdHDVzOmp7JFIhfPwtt',
+        cache: {
+          clear: 'auto',
+          type: 'memory'
+        }
+      })
+
+      client.get(`cdn/stories`, {
+        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft',
+        start_with: 'articles/'
+      }).then(response => {
+          console.log("movimpact res ",response)
+          this.setState({ articles: response.data.stories});
+      })
+    /* axios.get("https://jsonplaceholder.typicode.com/photos").then(response => {
+      this.setState({ articles: response.data.slice(0, 18) });
+    }); */
+  }
+  render() {
+    const articles = this.state.articles.map(article => {
+      return (
+        <div className="story" key={article.id}>
+          <img src={article.content.thumbnail} alt="another" />
+            <div className="story__overlay">
+            <h4 className="story__overlay-title">{article.content.title}</h4>
+                <div className="story__overlay-text">
+                {article.content.summary}
+                </div>
+            </div>   
+        </div>
+      );
+    });
+    return (
+      <div>
+        <div className="article-section">{articles}</div>
+      </div>
+    );
+  }
+}
+
+export default Article;
--- src/components/home/home-container.js
+++ src/components/home/home-container.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from './../layout/navbar';
-
+import Article from '../article/article'
 const HomeContainer = (props) => {
   return (
-    <div>
+    <div className="home-container">
       <Navbar {...props}></Navbar>
+      <Article></Article>
     </div>
   )
 };
